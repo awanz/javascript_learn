eb24503588db7ab5e939a58072b1ec1ecc4c796d
awan <me.awan.es@gmail.com>
2022-11-18 08:48:58 +0700

    operator arithematic

---
+++ 0005_operator.js
@@ -0,0 +1,28 @@
+/*
+    Arithmetic Operators
+    +
+    -
+    *
+    /
+    **
+    %
+    ++
+    --
+*/
+
+let a = 2;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a ** b);
+console.log(a % b);
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a);
+console.log(a--);
+console.log(a); // 3
+console.log(--a); // 2
+console.log(a); // 2

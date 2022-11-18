a7e4318a466adbb27024bf564d787dfc197cbb08
awan <me.awan.es@gmail.com>
2022-11-18 07:50:20 +0700

    variable

---
+++ 0004_variable.js
@@ -0,0 +1,17 @@
+let numb = 16;
+console.log(numb);
+
+let $numb = 17;
+console.log($numb);
+
+let _numb = 18;
+console.log(_numb);
+
+let firstName, FirstName;
+firstName = "John";
+FirstName = "Doe";
+console.log(firstName);
+console.log(FirstName);
+
+// hyphens cant use for variable
+// let first-name = "John";

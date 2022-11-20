9a680df32ed865faa6f945cdeb5c8179bb28226a
awan <me.awan.es@gmail.com>
2022-11-20 18:19:50 +0700

    update operator and add function

--- 0005_operator.js
+++ 0005_operator.js
@@ -26,3 +26,18 @@ console.log(a--);
 console.log(a); // 3
 console.log(--a); // 2
 console.log(a); // 2
+
+console.log(5^1);
+
+objectVariable = {
+    name: 'Hermawan',
+    age: 25,
+}
+
+objectVariable2 = {
+    name: 'Hermawan',
+    age: 25,
+}
+
+let test = objectVariable instanceof objectVariable2;
+console.log(test);
---
+++ 0006_function.js
@@ -0,0 +1,11 @@
+function printHello(){
+    console.log("Hello, World!");
+}
+
+printHello();
+
+function bio(name, age){
+    console.log("My name is " + name + " and I am " + age + " years old.");
+}
+
+bio("John", 25);

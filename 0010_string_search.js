560b3624267354542962f19ea66a39e678de6512
awan <me.awan.es@gmail.com>
2022-11-23 06:18:34 +0700

    update string search

--- 0010_string_search.js
+++ 0010_string_search.js
@@ -1,5 +1,11 @@
-let tes = "Hello World, I will learn something new today";
-let IO = tes.indexOf("World");
-console.log(IO);
+let text = "Hello World, I will learn something new today";
 
-console.log(tes.lastIndexOf("new"));
+console.log(text.indexOf("World"));
+console.log(text.lastIndexOf("new"));
+console.log(text.search(/learn/));
+console.log(text.match(/learn/));
+console.log(text.matchAll(/learn/g));
+console.log(text.includes("learn"));
+console.log(text.startsWith("Hello"));
+console.log(text.endsWith("today"));
+console.log("End");

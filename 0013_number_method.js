41315c40f0d4ca1403cba8873304bbc87553e878
awan <me.awan.es@gmail.com>
2022-11-24 07:11:39 +0700

    number method

---
+++ 0013_number_method.js
@@ -0,0 +1,13 @@
+let number = 2.1508;
+console.log(typeof number);
+let numberString = number.toString();
+console.log(typeof numberString);
+
+console.log(number.toExponential(2));
+
+console.log(number.toFixed(2));
+console.log(number.toPrecision(2));
+console.log(number.valueOf());
+console.log(Number(number));
+console.log(parseInt(number));
+console.log(parseFloat(number));

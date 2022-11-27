181b815039cafc11d25a0583a5e6ca7633b63e90
awan <me.awan.es@gmail.com>
2022-11-27 18:28:40 +0700

    date set method

---
+++ 0022_date_set_method.js
@@ -0,0 +1,26 @@
+const date = new Date(1000000000000);
+console.log(date);
+
+date.setDate(25);
+console.log(date);
+
+date.setFullYear(2022);
+console.log(date);
+
+date.setHours(22);
+console.log(date);
+
+date.setMilliseconds(100);
+console.log(date);
+
+date.setMinutes(30);
+console.log(date);
+
+date.setMonth(8);
+console.log(date);
+
+date.setSeconds(50);
+console.log(date);
+
+date.setTime(1000000000000);
+console.log(date);

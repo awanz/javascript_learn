c1dfca286b2c8d7e5a9731588417f73fa2fe1370
awan <me.awan.es@gmail.com>
2022-11-27 08:26:26 +0700

    array const

---
+++ 0018_array_const.js
@@ -0,0 +1,12 @@
+const name = ["awan", "khan", "ali"]; // array cant be reassigned
+console.log(name);
+
+// element can be reassigned
+name[1] = "dina";
+console.log(name);
+
+{
+    const name = ["star", "june", "tin"];
+    console.log(name);
+}
+console.log(name);

2273b4d6bbee79b6cd0c9ff5b6809a18134e9d09
awan <me.awan.es@gmail.com>
2022-11-28 22:08:50 +0700

    update math

--- 0023_math.js
+++ 0023_math.js
@@ -1,1 +1,35 @@
+// Math Properties (Constants)
 console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+
+// Math Methods
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.4));
+console.log(Math.floor(4.7));
+console.log(Math.trunc(4.7));
+console.log(Math.sign(-5));
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(9));
+console.log(Math.abs(-5));
+console.log(Math.sin(0));
+console.log(Math.cos(0));
+console.log(Math.tan(0));
+console.log(Math.asin(0));
+console.log(Math.acos(1));
+console.log(Math.atan(0));
+console.log(Math.atan2(0, 1));
+console.log(Math.exp(1));
+console.log(Math.expm1(1));
+console.log(Math.log(1));
+console.log(Math.log1p(0));
+console.log(Math.log10(100));
+console.log(Math.log2(4));
+console.log(Math.cbrt(8));
+console.log(Math.hypot(3, 4));
+console.log(Math.random());

b4c304afb29af7fbb0ae101622bbf15e6d339c49
awan <me.awan.es@gmail.com>
2022-11-29 19:42:47 +0700

    if else

---
+++ 0026_if_else.js
@@ -0,0 +1,9 @@
+let nilaiUjian = 83;
+
+if (nilaiUjian > 80) {
+  console.log("Selamat, anda lulus");
+} else if (nilaiUjian === 80) {
+  console.log("Cukup Lulus");
+} else {
+  console.log("Maaf, anda tidak lulus");
+}

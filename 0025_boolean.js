36dbf6c9329ab80155c7665841d64084f7a70330
awan <me.awan.es@gmail.com>
2022-11-29 19:39:51 +0700

    boolean

--- 0025_boolean.js
+++ 0025_boolean.js
@@ -1,1 +1,5 @@
 console.log(Boolean(1 < 4)); // false
+console.log(Boolean(1)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean("")); // false
+console.log(Boolean("Hello")); // true

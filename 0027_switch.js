c05d2c9f286e889d49fcb997748c4910241d7357
awan <me.awan.es@gmail.com>
2022-11-30 18:08:02 +0700

    switch

---
+++ 0027_switch.js
@@ -0,0 +1,14 @@
+const number = 10;
+
+switch (number) {
+    case 10:
+        console.log('great');
+        break;
+    case 5:
+        console.log('good');
+        break;
+    default: // run if no match
+        console.log('ok');
+        break;
+}
+

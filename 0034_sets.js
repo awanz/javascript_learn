92a67636dd74f290d1ed32009935add682748991
awan <me.awan.es@gmail.com>
2022-12-04 09:30:25 +0700

    sets

--- 0034_sets.js
+++ 0034_sets.js
@@ -5,3 +5,14 @@ console.log(letters);
 letters.add("d");
 
 console.log(letters);
+
+console.log(letters.values());
+letters.delete("b");
+console.log(letters);
+console.log(letters.has("c"));
+console.log(letters.size);
+letters.forEach(element => {
+    console.log(element);
+});
+letters.clear();
+console.log(letters);

eb59dec4f7ba5e92eb39abaaf9e0d25c7f18b9f3
awan <me.awan.es@gmail.com>
2022-12-06 21:34:55 +0700

    maps

--- 0035_maps.js
+++ 0035_maps.js
@@ -11,3 +11,17 @@ console.log(fruits);
 
 const valueJeruk = fruits.get("jeruk");
 console.log(valueJeruk);
+
+fruits.delete("melon");
+console.log(fruits);
+
+console.log(fruits.size);
+console.log(fruits.has("apel"));
+
+console.log(fruits.keys());
+console.log(fruits.values());
+console.log(fruits.entries());
+
+fruits.forEach((value, key) => {
+    console.log(key, value);
+});

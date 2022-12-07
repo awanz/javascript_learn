dc889e52e057e1175d1bc23b93d2fd44dd16c721
awan <me.awan.es@gmail.com>
2022-12-07 08:25:56 +0700

    typeof

---
+++ 0036_typeof.js
@@ -0,0 +1,10 @@
+console.log(typeof "John"); // string
+console.log(typeof 3.14); // number
+console.log(typeof NaN); // number
+console.log(typeof false); // boolean
+console.log(typeof [1,2,3,4]); // object
+console.log(typeof {name:'John', age:34}); // object
+console.log(typeof new Date()); // object
+console.log(typeof function () {}); // function
+console.log(typeof myCar); // undefined
+console.log(typeof null); // object

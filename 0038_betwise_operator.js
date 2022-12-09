1f7b99106d5b6211e7a7d709f992e009e6e8c88f
awan <me.awan.es@gmail.com>
2022-12-09 17:42:56 +0700

    betwise

---
+++ 0038_betwise_operator.js
@@ -0,0 +1,28 @@
+// AND
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+// OR
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+// NOT
+console.log(!true); // false
+console.log(!false); // true
+
+// XOR
+console.log(true ^ true); // false
+console.log(true ^ false); // true
+console.log(false ^ true); // true
+console.log(false ^ false); // false
+
+console.log(~true); // -2
+console.log(~false); // -1
+
+console.log(5 << 1); // 10
+console.log(5 >> 1); // 2
+console.log(5 >>> 1); // 2

e4f59bd712df0eb094f2fef96f7001651cef622c
awan <me.awan.es@gmail.com>
2022-12-10 20:05:38 +0700

    this keyboard

---
+++ 0043_this_keyboard.js
@@ -0,0 +1,13 @@
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+};
+
+console.log(person.fullName());
+
+let x = this
+console.log(x);

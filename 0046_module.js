3860f77f1a4808d4195b0d764b7452253b11035f
awan <me.awan.es@gmail.com>
2022-12-12 19:11:09 +0700

    add comment

--- 0046_module.js
+++ 0046_module.js
@@ -1,2 +1,4 @@
 import { name, age } from "person";
 console.log(name, age); // Jesse 40
+
+// harus nambahain type di package.json yaitu module
--- 0048_debugger.js
+++ 0048_debugger.js
@@ -1,3 +1,5 @@
 console.log("before debugger");
 debugger;
 console.log("after debugger");
+
+// hanya jalan di debugger inspect element
